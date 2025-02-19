ca9dfd28a69926e64f4bf9ebab5ac3e995b2d332
Inferno-kN <aldosovich@bk.ru>
2025-02-19 19:05:09 +0300

    сделали инициализацию полей дудлика в Doodlik.py и обозначили параметры движениям

--- .venv/Doodlik.py
+++ .venv/Doodlik.py
@@ -4,6 +4,17 @@ class Doodle:
 
     def __init__(self, screen):
         #здесь будет инициализация нашего дудлика
+        self.screen = screen
+        self.image = pygame.image.load("images/doodlik.png")  # загрузка нашего дудлика на экран
+        self.rectangle = self.image.get_rect()  # получение картинки как прямоугольник
+        self.screen_rectangle = screen.get_rect()
+        self.rectangle.centerx = self.screen_rectangle.centerx
+        self.rectangle.bottom = self.screen_rectangle.bottom
+
+        # теперь обозначим параметры движения у дудлика
+        self.speed = 5
+        self.gravity = 0.5
+
 
 
 
