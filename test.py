7f795dd46c44b107ea4d8fa8f61ecbb110ae8d4d
Inferno-kN <aldosovich@bk.ru>
2025-02-20 21:04:35 +0300

    Было добавлено ограничение фпс в главном цикле игры

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from Doodlik import Doodle
 #Цвета
 WHITE = 255, 255, 255
 BLACK = 0, 0, 0
+FPS = 90
 
 
 def run():
@@ -17,13 +18,15 @@ def run():
 
 
     while True:
+        clock.tick(FPS)
         for event in pygame.event.get(): #получаем события пользователя (по типу нажал на клавишу и тд)
             if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
 
         screen.fill(WHITE) #установили белый фон
         doodle.output() # отрисовка на главный экран нашего дудлика
         pygame.display.flip() # обновление экрана
-        clock.tick(90) #ставим ограничение на фпс до 90
+        clock.tick(FPS) #ставим ограничение на фпс до 90
 
 run()
